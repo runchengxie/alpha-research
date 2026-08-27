80246544e53ed41780f6df73cee46875ade1c8c1
Richard <709090316@qq.com>
2026-08-27 23:21:58 +0800

    refactor: remove StyleReplica portfolio exports from alpha

--- src/alpha_research/style_replica/__init__.py
+++ src/alpha_research/style_replica/__init__.py
@@ -1,4 +1,4 @@
-"""Rule-based StyleReplica signal and portfolio construction package."""
+"""Rule-based StyleReplica alpha signal research package."""
 
 from .factors import (
     compute_beta_factor,
@@ -9,13 +9,6 @@ from .factors import (
     compute_size_factor,
     compute_vol_convergence_factor,
     compute_volume_activity_factor,
-)
-from .portfolio import (
-    StyleReplicaPortfolioConfig,
-    build_style_replica_positions,
-    compute_daily_changes,
-    compute_daily_exposure,
-    compute_style_exposure_summary,
 )
 from .resvol import compute_resvol_factor
 from .score_a import compute_score_a
@@ -31,12 +24,8 @@ from .universe import filter_style_replica_universe
 __all__ = [
     "AI_HARDWARE_THEME_QUOTAS",
     "StyleReplicaConfig",
-    "StyleReplicaPortfolioConfig",
     "StyleReplicaSignalGenerator",
-    "build_style_replica_positions",
     "compute_beta_factor",
-    "compute_daily_changes",
-    "compute_daily_exposure",
     "compute_hermite_stability_factor",
     "compute_industry_momentum",
     "compute_liquidity_factor",
@@ -45,7 +34,6 @@ __all__ = [
     "compute_score_a",
     "compute_score_b",
     "compute_size_factor",
-    "compute_style_exposure_summary",
     "compute_vol_convergence_factor",
     "compute_volume_activity_factor",
     "filter_style_replica_universe",
